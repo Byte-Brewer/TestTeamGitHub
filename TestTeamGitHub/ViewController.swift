7c57d389371a452979966256f1e7a5640f242a12
Nazar_P <nazar.p@icloud.com>
2017-02-14 13:04:27 +0200

    add segmented controller

--- TestTeamGitHub/ViewController.swift
+++ TestTeamGitHub/ViewController.swift
@@ -9,17 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segContOutlet: UISegmentedControl!
+    @IBAction func segmentController(_ sender: UISegmentedControl, forEvent event: UIEvent) {
+        switch segContOutlet.selectedSegmentIndex {
+        case 0:
+            print("0")
+        case 1:
+            print("1")
+        default: break
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
